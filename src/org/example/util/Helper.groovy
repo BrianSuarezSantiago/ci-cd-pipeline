72c5a199607c1b8bdcf41391598c463c3741c913
BrianSuarezSantiago <briansuarezsantiago>
2024-11-20 08:19:28 +0000

    initial

---
+++ src/org/example/util/Helper.groovy
@@ -0,0 +1,7 @@
+package org.example.util
+
+class Helper {
+    static String saludar(String nombre) {
+        return "Hola, ${nombre}!"
+    }
+}
---
+++ vars/call.groovy
@@ -0,0 +1,3 @@
+def call(String name = "default") {
+    echo "Hola, ${name}! Este es un step genérico."
+}
